c1534d78ddb1e951dd31502401df9a101c4cd253
zim <renegadez@tut.by>
2023-12-19 09:49:33 +0300

    Use Factory

--- app/src/main/java/com/example/dependencyinjectionstart/example2/presentation/MainActivity.kt
+++ app/src/main/java/com/example/dependencyinjectionstart/example2/presentation/MainActivity.kt
@@ -3,7 +3,7 @@ package com.example.dependencyinjectionstart.example2.presentation
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.dependencyinjectionstart.R
-import com.example.dependencyinjectionstart.example2.di.DaggerApplicationComponent
+import com.example.dependencyinjectionstart.example2.ExampleApp
 import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
@@ -12,10 +12,7 @@ class MainActivity : AppCompatActivity() {
     lateinit var viewModel: ExampleViewModel
 
     private val component by lazy {
-        DaggerApplicationComponent.builder()
-            .context(application)
-            .timeMillis(System.currentTimeMillis())
-            .build()
+        (application as ExampleApp).component
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
